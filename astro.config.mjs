f132a28b003ae0a4801fbd189e43d5346001c5aa
VlamJasper <91375339+VlamJasper@users.noreply.github.com>
2023-01-27 14:06:52 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: 'wSY5hr0vVjJZgXyi6nDZxwtt',
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
